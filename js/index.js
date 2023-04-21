bad54c631143b347d8e5a66aedc545d3fa8fcdec
Kevin Bailey <kgbailey02@gmail.com>
2023-04-21 12:48:22 -0600

    refactored, added a new theme, and made it mobile responsive

--- js/index.js
+++ js/index.js
@@ -6,6 +6,9 @@ const aboutContent = document.getElementById("about");
 const profileContent = document.getElementById("profile");
 const newFormWindow = document.getElementById("new-person-menu");
 const aboutDotPage = document.getElementById('about-dot-page');
+const mobileProfileLink = document.getElementById('profile-mobile-link');
+const mobileAboutLink = document.getElementById('about-mobile-link');
+
 
 //about link routing
 aboutLink.addEventListener('click', () =>{
@@ -17,6 +20,18 @@ aboutLink.addEventListener('click', () =>{
     aboutContent.style.display = 'block';
     homeContent.style.display = "none";
     profileContent.style.display = "none";
+})
+
+mobileAboutLink.addEventListener('click', () =>{
+  aboutLink.style.color = "rgb(255, 130, 174)";
+  homeLink.style.color = 'white';
+  profileLink.style.color = 'white';
+  newFormWindow.style.display = "none";
+  newFormWindow.classList.remove("show");
+  aboutContent.style.display = 'block';
+  homeContent.style.display = "none";
+  profileContent.style.display = "none";
+  mobileLinks.classList.toggle('active');
 })
 
 //home link routing
@@ -42,6 +57,18 @@ profileLink.addEventListener('click', () =>{
     homeContent.style.display = "none";
 })
 
+mobileProfileLink.addEventListener('click', () =>{
+  profileLink.style.color = "rgb(255, 130, 174)";
+  homeLink.style.color = 'white';
+  aboutLink.style.color = 'white';
+  newFormWindow.style.display = "none";
+  newFormWindow.classList.remove("show");
+  profileContent.style.display = 'block';
+  aboutContent.style.display = "none";
+  homeContent.style.display = "none";
+  mobileLinks.classList.toggle('active');
+})
+
 //importing and working the json user information
 fetch('user.json')
     .then(response => response.json())
@@ -62,7 +89,7 @@ fetch('user.json')
         const profileAboutContainer = document.getElementById('profile-about-container');
         const aboutContent = document.getElementById('about-content')
 
-        aboutDotPage.innerText = "Welcome, " + name + " to Flirtfolio! This is the main page. Each person's name is shown, along with a colored dot that represents their chosen theme color. When hovering over a person's name, an X icon appears in the top right corner. Clicking this icon removes the person from your list of people. If you want to add a new person to your list, click the Edit button at the bottom of the page to load a new form."
+        aboutDotPage.innerText = "Welcome, " + name + " to Flirtfolio! This is the main page. Each person's name is shown, along with a colored dot that represents their chosen theme color. When hovering over a person's name, an X icon appears in the top right corner. Clicking this icon removes the person from your list of people. If you want to add a new person to your list, click the Edit button to load a new form. We hope you enjoy using FlirtFolio!"
 
         topProfileInfo.textContent = name;
         bottomProfileInfo.textContent = age + " / " + location;
@@ -218,5 +245,202 @@ submitButton.addEventListener('click', (event) => {
         });
 });
 
+const themeCheckbox = document.getElementById('theme-checkbox');
+const styleLink = document.getElementById('style-link');
 
+
+
+
+//hamburger menu stuff
+
+const hamburgerMenu = document.getElementById('hamburger-menu');
+const mobileLinks = document.querySelector('.mobile-links');
+
+hamburgerMenu.addEventListener('click', () => {
+  console.log("click");
+  mobileLinks.classList.toggle('active');
+});
+
+
+
+
+themeCheckbox.addEventListener('change', () => {
+  if (themeCheckbox.checked) {
+    console.log("changed to 2");
+    styleLink.href = "/css/styles2.css";
+    fetch('user2.json')
+    .then(response => response.json())
+    .then(data => {
+
+        const name = data.name;
+        const age = data.age;
+        const gender = data.gender;
+        const location = data.location
+        const interests = data.interests;
+        const about = data.about;
+        const people = data.people;
+        const container = document.getElementById('people-container');
+        const profileContainer = document.getElementById('profile');
+        const profileInfo = document.getElementById('profile-info');
+        const topProfileInfo = document.createElement('span');
+        const bottomProfileInfo = document.createElement('span');
+        const profileAboutContainer = document.getElementById('profile-about-container');
+        const aboutContent = document.getElementById('about-content')
+
+        aboutDotPage.innerText = "Welcome, " + name + " to Flirtfolio! This is the main page. Each person's name is shown, along with a colored dot that represents their chosen theme color. When hovering over a person's name, an X icon appears in the top right corner. Clicking this icon removes the person from your list of people. If you want to add a new person to your list, click the Edit button to load a new form. We hope you enjoy using FlirtFolio!"
+
+        topProfileInfo.textContent = name;
+        bottomProfileInfo.textContent = age + " / " + location;
+        topProfileInfo.classList.add("profile-name");
+        bottomProfileInfo.classList.add("age-and-location");
+        profileInfo.innerHTML = ''
+        profileInfo.appendChild(topProfileInfo);
+        profileInfo.appendChild(bottomProfileInfo);
+        aboutContent.innerText = about;
+
+        const interestsContainer = document.getElementById("interests-list");
+        interestsContainer.innerHTML = '';
+        interests.forEach(interest => {
+            const interestItem = document.createElement("li");
+            interestItem.innerText = interest;
+            interestsContainer.appendChild(interestItem);
+        });
+
+        container.innerHTML = '';
+        people.forEach(person => {
+          const firstName = person.firstName;
+          const lastName = person.lastName;
+          const color = person.color;
+          const numOfDates = person.numOfDates;
+
+          const personDiv = document.createElement('div');
+          personDiv.classList.add('person-div');
+
+          const colorSpan = document.createElement('span');
+          colorSpan.classList.add('dotSpan');
+          colorSpan.style.backgroundColor = color;
+          personDiv.appendChild(colorSpan);
+
+          const nameSpan = document.createElement('div');
+          nameSpan.textContent = firstName + ' ' + lastName;
+          personDiv.appendChild(nameSpan);
+
+          const closeButton = document.createElement('button');
+          closeButton.innerHTML = "X";
+          closeButton.classList.add('close-button');
+          closeButton.style.opacity = 0;
+          personDiv.appendChild(closeButton);
+
+          closeButton.addEventListener('click', () => {
+            personDiv.classList.add('leave');
+            setTimeout(function(){personDiv.remove();},700);
+          });
+        
+          
+          personDiv.addEventListener('mouseenter', () => {
+            closeButton.style.opacity = 1;
+          });
+          personDiv.addEventListener('mouseleave', () => {
+            closeButton.style.opacity = 0;
+          });
+          
+          container.appendChild(personDiv);
+        })
+    });
+
+    profileImage = document.getElementById("profile-image");
+    profileImage.src = "/img/actress-Margot-Robbie-Australian-2018.png";
+    editIcon.src = "/img/add-button.svg"
+
+
+
+ 
+  } else {
+    styleLink.href = "/css/styles.css"; 
+    fetch('user.json')
+    .then(response => response.json())
+    .then(data => {
+
+        const name = data.name;
+        const age = data.age;
+        const gender = data.gender;
+        const location = data.location
+        const interests = data.interests;
+        const about = data.about;
+        const people = data.people;
+        const container = document.getElementById('people-container');
+        const profileContainer = document.getElementById('profile');
+        const profileInfo = document.getElementById('profile-info');
+        const topProfileInfo = document.createElement('span');
+        const bottomProfileInfo = document.createElement('span');
+        const profileAboutContainer = document.getElementById('profile-about-container');
+        const aboutContent = document.getElementById('about-content')
+       
+        aboutDotPage.innerText = "Welcome, " + name + " to Flirtfolio! This is the main page. Each person's name is shown, along with a colored dot that represents their chosen theme color. When hovering over a person's name, an X icon appears in the top right corner. Clicking this icon removes the person from your list of people. If you want to add a new person to your list, click the Edit button to load a new form. We hope you enjoy using FlirtFolio!"
+        topProfileInfo.textContent = name;
+        bottomProfileInfo.textContent = age + " / " + location;
+        topProfileInfo.classList.add("profile-name");
+        bottomProfileInfo.classList.add("age-and-location");
+        profileInfo.innerHTML = ''
+        profileInfo.appendChild(topProfileInfo);
+        profileInfo.appendChild(bottomProfileInfo);
+        aboutContent.innerText = about;
+        
+        const interestsContainer = document.getElementById("interests-list");
+        interestsContainer.innerHTML = '';
+        interests.forEach(interest => {
+            const interestItem = document.createElement("li");
+            interestItem.innerText = interest;
+            interestsContainer.appendChild(interestItem);
+        });
+
+        container.innerHTML = '';
+        people.forEach(person => {
+          const firstName = person.firstName;
+          const lastName = person.lastName;
+          const color = person.color;
+          const numOfDates = person.numOfDates;
+
+          const personDiv = document.createElement('div');
+          personDiv.classList.add('person-div');
+
+          const colorSpan = document.createElement('span');
+          colorSpan.classList.add('dotSpan');
+          colorSpan.style.backgroundColor = color;
+          personDiv.appendChild(colorSpan);
+
+          const nameSpan = document.createElement('div');
+          nameSpan.textContent = firstName + ' ' + lastName;
+          personDiv.appendChild(nameSpan);
+
+          const closeButton = document.createElement('button');
+          closeButton.innerHTML = "X";
+          closeButton.classList.add('close-button');
+          closeButton.style.opacity = 0;
+          personDiv.appendChild(closeButton);
+
+          closeButton.addEventListener('click', () => {
+            personDiv.classList.add('leave');
+            setTimeout(function(){personDiv.remove();},700);
+          });
+        
+          
+          personDiv.addEventListener('mouseenter', () => {
+            closeButton.style.opacity = 1;
+          });
+          personDiv.addEventListener('mouseleave', () => {
+            closeButton.style.opacity = 0;
+          });
+          
+          container.appendChild(personDiv);
+        })
+    });
+
+    profileImage = document.getElementById("profile-image");
+    profileImage.src = "/img/profile-image.jpg";
+    editIcon.src = "/img/edit-icon.png"
+
+    
+  }
+});
   
